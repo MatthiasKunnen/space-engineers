7d0a918b716d925c029d3e3659be5ba67745297e
Matthias Kunnen <matthias.kunnen@gmail.com>
2018-10-28 19:44:48 +0100

    Pointed ship upward during ascend

--- scripts/launch-control/launch-control.cs
+++ scripts/launch-control/launch-control.cs
@@ -24,6 +24,8 @@ double gravityTreshold = 0; // Specifies at how many gyro script will stop, 0g b
 bool reachedTargetSpeedOnce;
 bool isPreviousCorrectionIncrease;
 
+GyroController gyroController;
+List<IMyGyro> gyros;
 List<IMyThrust> thrusters;
 IMyShipController controlBlock;
 IMyTimerBlock timer;
@@ -45,6 +47,8 @@ void Main(string args = "START") {
     }
 
     thrusters = GetBlocksInGroup<IMyThrust>(ascentThrustersGroup);
+    gyros = GetBlocksOfType<IMyGyro>();
+    gyroController = new GyroController(controlBlock, gyros, Base6Directions.Direction.Down);
 
     var gravity = controlBlock.GetNaturalGravity();
     var gravityLength = gravity.Length();
@@ -76,6 +80,7 @@ void Main(string args = "START") {
 
     if (!escaped) {
         ApplyThrust();
+        gyroController.Align(gravity);
         angle = Math.Acos(
             Vector3D.Dot(
                 Vector3D.Normalize(controlBlock.GetNaturalGravity()),
@@ -91,7 +96,7 @@ void Main(string args = "START") {
 
     if (args == "STOP" || escaped) {
         thrusters.ForEach(t => t.SetValueFloat("Override", 0));
-
+        gyroController.Stop();
         timer.ApplyAction("Stop");
         ClearOutput();
         WriteLine("Launch control ended.");
@@ -169,3 +174,134 @@ List<T> GetBlocksInGroup<T>(string groupName) where T : class {
 
     return null;
 }
+
+List<T> GetBlocksOfType<T>() where T : class {
+    var result = new List<T>();
+    var blocks = new List<IMyTerminalBlock>();
+    GridTerminalSystem.GetBlocksOfType<T>(blocks);
+
+    for(var i = 0; i < blocks.Count; i++) {
+        result.Add((T)blocks[i]);
+    }
+
+    return result;
+}
+
+void SetDampeners(bool enabled) {
+    if (controlBlock.DampenersOverride != enabled) {
+        controlBlock.GetActionWithName("DampenersOverride").Apply(controlBlock);
+    }
+}
+
+#region Gyro
+partial class GyroController: MyGridProgram {
+    // Originally from: http://forums.keenswh.com/threads/aligning-ship-to-planet-gravity.7373513/#post-1286885461
+
+    /// <summary>
+    /// The alignment of the ship
+    /// </summary>
+    Base6Directions.Direction alignment;
+
+    /// <summary>
+    /// How much power to use. 0 to 1.0
+    /// </summary>
+    double powerCoefficient;
+
+    /// <summary>
+    /// The ship controller used
+    /// </summary>
+    IMyShipController gyroControl;
+
+    /// <summary>
+    /// The list of gyros to use for aiming
+    /// </summary>
+    List<IMyGyro> gyros;
+
+    /// <summary>
+    /// How tight to maintain aim. Lower is tighter. Default is 0.01f
+    /// </summary>
+    float minAngleRad = 0.01f;
+
+    public GyroController(
+        IMyShipController gyroControl,
+        List<IMyGyro> gyros,
+        Base6Directions.Direction alignment,
+        double powerCoefficient = 0.3
+    ) {
+        this.gyroControl = gyroControl;
+        this.gyros = gyros;
+        this.alignment = alignment;
+        this.powerCoefficient = powerCoefficient;
+    }
+
+    /// <summary>
+    /// Align the ship/grid with the given vector.
+    /// </summary>
+    /// <param name="alignment">The alignment of the ship.</param>
+    /// <param name="vDirection">the vector to aim for.</param>
+    /// <returns>true if aligned. Meaning the angle of error is less than minAngleRad</returns>
+    public bool Align(Vector3D vDirection, Base6Directions.Direction? alignment = null) {
+        bool aligned = true;
+        Matrix orientation;
+        gyroControl.Orientation.GetMatrix(out orientation);
+
+        Vector3D down = orientation.GetDirectionVector(alignment ?? this.alignment);
+        vDirection.Normalize();
+
+        gyros.ForEach(gyro => {
+            gyro.Orientation.GetMatrix(out orientation);
+
+            var localCurrent = Vector3D.Transform(down, MatrixD.Transpose(orientation));
+            var localTarget = Vector3D.Transform(vDirection, MatrixD.Transpose(gyro.WorldMatrix.GetOrientation()));
+
+            // Since the gyro ui lies, we are not trying to control yaw, pitch, roll
+            // but rather we need a rotation vector (axis around which to rotate)
+            var rot = Vector3D.Cross(localCurrent, localTarget);
+            double dot2 = Vector3D.Dot(localCurrent, localTarget);
+            double ang = rot.Length();
+            ang = Math.Atan2(ang, Math.Sqrt(Math.Max(0.0, 1.0 - ang * ang)));
+            if (dot2 < 0) ang = Math.PI - ang; // Compensate for >+/-90
+            if (ang < minAngleRad) {
+
+                gyro.GyroOverride = false;
+                return;
+            }
+
+            float yawMax = (float)(2 * Math.PI);
+
+            double ctrlLvl = yawMax * (ang / Math.PI) * powerCoefficient;
+
+            ctrlLvl = Math.Min(yawMax, ctrlLvl);
+            ctrlLvl = Math.Max(0.01, ctrlLvl);
+            rot.Normalize();
+            rot *= ctrlLvl;
+
+            float pitch = -(float)rot.X;
+            gyro.Pitch = pitch;
+
+            float yaw = -(float)rot.Y;
+            gyro.Yaw = yaw;
+
+            float roll = -(float)rot.Z;
+            gyro.Roll = roll;
+
+            gyro.GyroOverride = true;
+
+            aligned = false;
+        });
+        return aligned;
+    }
+
+    /// <summary>
+    /// Turns off all overrides on controlled Gyros
+    /// </summary>
+    public void Stop() {
+        gyros.ForEach(gyro => {
+            gyro.GetActionWithName("OnOff_On").Apply(gyro);
+            if (gyro.GyroOverride) {
+                gyro.GetActionWithName("Override").Apply(gyro);
+            }
+        });
+    }
+}
+#endregion
